d0bc2ebeb33810703690f898c9d538f4e3ee0922
Valentin Jevsejev <valentin.jevsejev@gmail.com>
2022-09-12 20:35:25 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out.log

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task19.task1907;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Считаем слово
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
+
+}
